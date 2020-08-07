2c6b20a144132a91ec3f701a93a859b733fadccb
mevlja's <sm9299@student.uni-lj.si>
2020-08-07 21:37:19 +0200

    Add Game view

---
+++ Bird_attack/app/src/main/java/com/example/birdattack/GameView.java
@@ -0,0 +1,27 @@
+package com.example.birdattack;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class GameView extends SurfaceView implements Runnable {
+
+    private Thread thread;
+
+    public GameView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public void resume(){
+
+    }
+
+
+    public void pause () {
+
+    }
+}
