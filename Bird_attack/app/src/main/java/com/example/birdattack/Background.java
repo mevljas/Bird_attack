2d62952e8c21794806a991aefbc9b12826dd905a
mevlja's <sm9299@student.uni-lj.si>
2020-08-08 17:30:45 +0200

    add Background constructor

--- Bird_attack/app/src/main/java/com/example/birdattack/Background.java
+++ Bird_attack/app/src/main/java/com/example/birdattack/Background.java
@@ -1,4 +1,19 @@
 package com.example.birdattack;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 public class Background {
+
+    int x, y;
+    Bitmap background;
+
+    Background (int screenX, int screenY, Resources res) {
+
+        background = BitmapFactory.decodeResource(res, R.drawable.background);
+
+        // resize
+        background = Bitmap.createScaledBitmap(background, screenX, screenY, false);
+    }
 }
