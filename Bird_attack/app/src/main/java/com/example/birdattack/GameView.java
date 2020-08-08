00186f960b888d1df086b950b98fe6f8195aaf1e
mevlja's <sm9299@student.uni-lj.si>
2020-08-08 17:41:14 +0200

    add Background update

--- Bird_attack/app/src/main/java/com/example/birdattack/GameView.java
+++ Bird_attack/app/src/main/java/com/example/birdattack/GameView.java
@@ -12,11 +12,14 @@ public class GameView extends SurfaceView implements Runnable {
     }
 
     private boolean isPlaying;
-
+    private int screenX, screenY;
     private Background background1, background2;
 
     public GameView(Context context, int screenX, int screenY) {
         super(context);
+
+        this.screenX = screenX;
+        this.screenY = screenY;
 
         background1 = new Background(screenX, screenY, getResources());
         background2 = new Background(screenX, screenY, getResources());
@@ -39,6 +42,19 @@ public class GameView extends SurfaceView implements Runnable {
     }
 
     private void update (){
+
+        background1.x -= 10;
+        background2.x -= 10;
+
+        if (background1.x + background1.background.getWidth() < 0) {
+
+            background1.x = screenX;
+        }
+
+        if (background2.x + background2.background.getWidth() < 0) {
+
+            background2.x = screenX;
+        }
 
     }
 
